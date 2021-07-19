3fd88594129d491d456d9dbbea8c085659adc34a
Sankalp <sankalpjain99@gmail.com>
2021-07-19 11:59:14 +0530

    Completed exchange-service by linking company-service using RestTemplate

--- backend/exchange-service/src/main/java/com/sankalp/exchangeservice/ExchangeServiceApplication.java
+++ backend/exchange-service/src/main/java/com/sankalp/exchangeservice/ExchangeServiceApplication.java
@@ -2,12 +2,19 @@ package com.sankalp.exchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExchangeServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
 	}
 
 }
