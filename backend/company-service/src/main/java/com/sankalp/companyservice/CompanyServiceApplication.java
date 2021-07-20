579de076bdfaaab0b73c90a2726d3e9b59f75a22
Sankalp <sankalpjain99@gmail.com>
2021-07-20 12:16:59 +0530

    Added Swagger to all the API's

--- backend/company-service/src/main/java/com/sankalp/companyservice/CompanyServiceApplication.java
+++ backend/company-service/src/main/java/com/sankalp/companyservice/CompanyServiceApplication.java
@@ -1,13 +1,43 @@
 package com.sankalp.companyservice;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CompanyServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.sankalp"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+	
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Company Microservice API",
+				"API Documentation for Company Microservice",
+				"1.0",
+				"Copyright- Sankalp Jain",
+				new springfox.documentation.service.Contact("Sankalp Jain", "https://sankalpjain99.github.io/", "sankalpjain99@gmail.com"),
+				"API License",
+				"https://github.com/sankalpjain99/Stock-Market-Application", Collections.emptyList());
 	}
 
 }
